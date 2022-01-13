102cc54ea5a6d7b30b84c896acd4cac44b2ccd43
fabioconejo <fabio.conejo@hotmail.com>
2022-01-13 17:12:24 -0300

    4 files added

---
+++ src/app/game-space/five-words/five-words.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'five-words',
+  templateUrl: './five-words.component.html',
+  styleUrls: ['./five-words.component.css'],
+})
+export class FiveWordsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/game-space/game-selector/game-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-selector',
+  templateUrl: './game-selector.component.html',
+  styleUrls: ['./game-selector.component.css'],
+})
+export class GameSelectorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
