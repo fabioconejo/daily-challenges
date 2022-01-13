639d2ecad04018fbf2dc4cbe4de3b5c46e924da7
fabioconejo <fabio.conejo@hotmail.com>
2022-01-13 17:58:30 -0300

    5 files added

---
+++ src/app/game-space/crossed/crossed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crossed',
+  templateUrl: './crossed.component.html',
+  styleUrls: ['./crossed.component.css']
+})
+export class CrossedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/game-space/passnumber/passnumber.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-passnumber',
+  templateUrl: './passnumber.component.html',
+  styleUrls: ['./passnumber.component.css']
+})
+export class PassnumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
