176c5fe081e4ef8a5a54edb969449f919d657108
fabioconejo <fabio.conejo@hotmail.com>
2022-01-21 15:56:21 -0300

    1 file modified

--- src/app/game-space/five-words/five-words.component.ts
+++ src/app/game-space/five-words/five-words.component.ts
@@ -12,20 +12,33 @@ export class FiveWordsComponent implements OnInit {
   currentWord: any = new Array(this.currentWordLength);
   lock: any = new Array(this.currentWordLength);
   index = 0;
+  indexList = 0;
 
   constructor() {}
 
   ngOnInit() {
-    document.documentElement.style.setProperty('--main-bg-color', '#1e1125');
-    document.documentElement.style.setProperty('--key-bg', '#180a20');
-    document.documentElement.style.setProperty('--slot-brd', '#483a50');
-    document.documentElement.style.setProperty('--slot-lock', '#483a50');
+    this.changeColors();
 
-    this.currentWord = this.wordTemplate.split('');
+    for (let i = 0; i < this.wordList.length; i++) {
+      this.wordList[i] = this.wordTemplate.split('');
+    }
+
+    this.currentWord = this.wordList[0];
 
     for (let i = 0; i < this.currentWord.length; i++) {
       this.lock[i] = this.currentWord[i] !== ' ';
     }
+  }
+
+  changeColors() {
+    document.documentElement.style.setProperty('--main-bg-color', '#1e1125');
+    document.documentElement.style.setProperty('--key-bg', '#180a20');
+    document.documentElement.style.setProperty('--slot-brd', '#483a50');
+    document.documentElement.style.setProperty('--slot-lock', '#483a50');
+  }
+
+  goToNextWord() {
+    this.indexList++;
   }
 
   typeLetter(letter: any) {
