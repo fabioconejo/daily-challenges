9dc6182c2ad3c6e6b1d1051f422a4db989ba1bc1
fabioconejo <fabio.conejo@hotmail.com>
2022-01-21 03:56:50 -0300

    2 files modified

--- src/app/game-space/game-elements/word/word.component.ts
+++ src/app/game-space/game-elements/word/word.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'game-word',
   templateUrl: './word.component.html',
   styleUrls: ['./word.component.css'],
 })
-export class WordComponent implements OnInit {
+export class WordComponent implements OnInit, OnChanges {
   @Input() word: string;
   @Input() wordLength: string;
   letters: any;
@@ -20,8 +26,10 @@ export class WordComponent implements OnInit {
     this.letters = new Array(+this.wordLength);
   }
 
-  ngOnChanges() {
-    console.log(this.word);
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.word) {
+      console.log(changes.word.currentValue);
+    }
   }
 
   public getStyles() {
