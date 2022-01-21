b6445e24112071306a803b33bbbe8d565ac73a16
fabioconejo <fabio.conejo@hotmail.com>
2022-01-20 22:01:03 -0300

    1 file modified

--- src/app/game-space/game-elements/word/word.component.ts
+++ src/app/game-space/game-elements/word/word.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-word',
+  selector: 'game-word',
   templateUrl: './word.component.html',
   styleUrls: ['./word.component.css']
 })
