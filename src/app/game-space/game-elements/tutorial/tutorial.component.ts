a1f34a2b34995ac57da698690858ba2fd9b7ff44
fabioconejo <fabio.conejo@hotmail.com>
2022-01-26 23:07:01 -0300

    3 files modified

--- src/app/game-space/game-elements/tutorial/tutorial.component.ts
+++ src/app/game-space/game-elements/tutorial/tutorial.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'tutorial',
   templateUrl: './tutorial.component.html',
-  styleUrls: ['./tutorial.component.css']
+  styleUrls: ['./tutorial.component.css'],
 })
 export class TutorialComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  closeOverlay() {
+    document.getElementsByClassName('overlay')[0].classList.add('closing');
+    setTimeout(function(){
+      document.getElementsByTagName('tutorial')[0].removeAttribute('open');
+      document.getElementsByClassName('overlay')[0].classList.remove('closing');
+  }, 150);
+    
   }
-
 }
