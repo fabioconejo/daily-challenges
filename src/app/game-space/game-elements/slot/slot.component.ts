bdcefe3c1fc427c532c9031ca0483b8bf31b5549
fabioconejo <fabio.conejo@hotmail.com>
2022-01-27 03:53:55 -0300

    2 files modified

--- src/app/game-space/game-elements/slot/slot.component.ts
+++ src/app/game-space/game-elements/slot/slot.component.ts
@@ -1,15 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'game-slot',
   templateUrl: './slot.component.html',
   styleUrls: ['./slot.component.css'],
 })
-export class SlotComponent implements OnInit {
+export class SlotComponent implements OnInit, OnChanges {
   @Input() letter: string = '';
   @Input() state: string;
+  animation: string = 'idle';
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnChanges() {
+    if (this.state !== 'empty') {
+      
+      this.animation = 'flip-in';
+      
+      setTimeout(function () {
+        this.animation = 'flip-out';  
+      }, 250);
+    }
+  }
 }
