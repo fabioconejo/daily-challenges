5727a5606bcdcbd961f3cd2d25283e03036cc7b7
fabioconejo <fabio.conejo@hotmail.com>
2022-01-31 00:39:44 -0300

    2 files modified

--- src/app/game-space/game-space.service.ts
+++ src/app/game-space/game-space.service.ts
@@ -1,5 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_mT-svtH83QG1luIE1lszWqMNRMljqf0",
+  authDomain: "daily-challenges-90763.firebaseapp.com",
+  databaseURL: "https://daily-challenges-90763-default-rtdb.firebaseio.com",
+  projectId: "daily-challenges-90763",
+  storageBucket: "daily-challenges-90763.appspot.com",
+  messagingSenderId: "436027376085",
+  appId: "1:436027376085:web:0ac1bac91d69943590bd5c",
+  measurementId: "G-YY5KE527FD"
+};
+
+initializeApp(firebaseConfig);
+
 @Injectable({
   providedIn: 'root',
 })
