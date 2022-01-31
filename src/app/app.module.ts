e8ca00eb059cd6c9250aee248595b8ffeaf7abf6
fabioconejo <fabio.conejo@hotmail.com>
2022-01-31 18:54:28 -0300

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { GameSpaceComponent } from './game-space/game-space.component';
 import { FiveWordsComponent } from './game-space/five-words/five-words.component';
@@ -17,23 +15,12 @@ import { KeyboardComponent } from './game-space/game-elements/keyboard/keyboard.
 import { SlotComponent } from './game-space/game-elements/slot/slot.component';
 import { WordComponent } from './game-space/game-elements/word/word.component';
 
-const config = {
-  apiKey: "AIzaSyD_mT-svtH83QG1luIE1lszWqMNRMljqf0",
-  authDomain: "daily-challenges-90763.firebaseapp.com",
-  databaseURL: "https://daily-challenges-90763-default-rtdb.firebaseio.com",
-  projectId: "daily-challenges-90763",
-  storageBucket: "daily-challenges-90763.appspot.com",
-  messagingSenderId: "436027376085",
-  appId: "1:436027376085:web:0ac1bac91d69943590bd5c",
-  measurementId: "G-YY5KE527FD"
-};
-
 @NgModule({
-  imports: [BrowserModule, FormsModule,
-    AngularFireModule.initializeApp(config),
-    AngularFirestoreModule, // firestore
-    AngularFireAuthModule, // auth
-    AngularFireStorageModule // storage
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   declarations: [
     AppComponent,
@@ -44,7 +31,7 @@ const config = {
     SlotComponent,
     WordComponent,
     TutorialComponent,
-    ToasterComponent
+    ToasterComponent,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/game-space/game-space.service.ts
+++ src/app/game-space/game-space.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameSpaceService {
-  constructor() {}
+  constructor(firestore: AngularFirestore) {}
 
   isAWord(word: string): boolean {
     return true;
