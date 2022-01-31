42f202ecd17907761a7a29d51c0acb0e2c3e43f9
fabioconejo <fabio.conejo@hotmail.com>
2022-01-31 20:33:16 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { GameSpaceComponent } from './game-space/game-space.component';
 import { FiveWordsComponent } from './game-space/five-words/five-words.component';
@@ -12,11 +13,14 @@ import { TutorialComponent } from './game-space/game-elements/tutorial/tutorial.
 import { KeyboardComponent } from './game-space/game-elements/keyboard/keyboard.component';
 import { SlotComponent } from './game-space/game-elements/slot/slot.component';
 import { WordComponent } from './game-space/game-elements/word/word.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   declarations: [
     AppComponent,
--- src/app/game-space/game-space.service.ts
+++ src/app/game-space/game-space.service.ts
@@ -1,13 +1,4 @@
 import { Injectable } from '@angular/core';
-import firebase from 'firebase/app';
-import { initializeApp } from 'firebase/app';
-
-import { environment } from '../../environments/environment';
-
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(environment.firebase);
-}
-
 
 @Injectable({
   providedIn: 'root',
