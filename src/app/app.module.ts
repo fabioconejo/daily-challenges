337a2a822cfaf29c297e9f1224b7367498f6bd5c
fabioconejo <fabio.conejo@hotmail.com>
2022-01-31 18:02:04 -0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 import { AppComponent } from './app.component';
 import { GameSpaceComponent } from './game-space/game-space.component';
 import { FiveWordsComponent } from './game-space/five-words/five-words.component';
@@ -12,8 +17,24 @@ import { KeyboardComponent } from './game-space/game-elements/keyboard/keyboard.
 import { SlotComponent } from './game-space/game-elements/slot/slot.component';
 import { WordComponent } from './game-space/game-elements/word/word.component';
 
+const config = {
+  apiKey: "AIzaSyD_mT-svtH83QG1luIE1lszWqMNRMljqf0",
+  authDomain: "daily-challenges-90763.firebaseapp.com",
+  databaseURL: "https://daily-challenges-90763-default-rtdb.firebaseio.com",
+  projectId: "daily-challenges-90763",
+  storageBucket: "daily-challenges-90763.appspot.com",
+  messagingSenderId: "436027376085",
+  appId: "1:436027376085:web:0ac1bac91d69943590bd5c",
+  measurementId: "G-YY5KE527FD"
+};
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
+  ],
   declarations: [
     AppComponent,
     GameSpaceComponent,
--- src/app/game-space/game-space.service.ts
+++ src/app/game-space/game-space.service.ts
@@ -1,17 +1,5 @@
 import { Injectable } from '@angular/core';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD_mT-svtH83QG1luIE1lszWqMNRMljqf0",
-  authDomain: "daily-challenges-90763.firebaseapp.com",
-  databaseURL: "https://daily-challenges-90763-default-rtdb.firebaseio.com",
-  projectId: "daily-challenges-90763",
-  storageBucket: "daily-challenges-90763.appspot.com",
-  messagingSenderId: "436027376085",
-  appId: "1:436027376085:web:0ac1bac91d69943590bd5c",
-  measurementId: "G-YY5KE527FD"
-};
-
-
 @Injectable({
   providedIn: 'root',
 })
